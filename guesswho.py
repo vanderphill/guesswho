5fc33a5683bdd759ab85fd5a789da4787bad446d
vanderphill <51059957+vanderphill@users.noreply.github.com>
2020-01-02 10:34:02 -0600

    added functionality to person guesses

--- guesswho.py
+++ guesswho.py
@@ -1,13 +1,31 @@
 from tkinter import *
 from PIL import Image, ImageTk
-x=15
-y=15
+from person import person
+
 root = Tk()
 root.title("Guess Who!")
 root.bg="yellow"
 topFrame=Frame(root)
 title=Label(topFrame,text="Guess Who!",font='Helvetica 22 bold',width="38",fg="brown",bg="yellow").pack()
 topFrame.grid(row=0,column=0,columnspan=3)
+
+x=15
+y=15
+
+start=ImageTk.PhotoImage(Image.open("Start.png"))
+mainFrame=Frame(root,bg="yellow",width="50",padx=42,pady=3)
+questionFrame=Frame(root,bg="yellow",padx=1,pady=31)
+questionFrame2=Frame(root,bg="yellow",padx=1,pady=31)
+choiceFrame=Frame(root,bg="yellow",pady=3)
+urchar= start
+choice=Label(choiceFrame,bg="yellow",padx=15,pady=15,image=urchar)
+yourcharcter = Label(choiceFrame,text ="Your Person",font='Helvetica 15 bold',bg="yellow",fg="brown")
+choice.pack()
+yourcharcter.pack()
+mainFrame.grid(row=1,column=0,columnspan=3)
+questionFrame.grid(row=2,column=0)
+choiceFrame.grid(row=2,column=1)
+questionFrame2.grid(row=2,column=2)
 
 
 Aleximage1=ImageTk.PhotoImage(Image.open("Alex.png"))
@@ -61,52 +79,90 @@ SusanimageX=ImageTk.PhotoImage(Image.open("SusanX.png"))
 TomimageX=ImageTk.PhotoImage(Image.open("TomX.png"))
 
 
-Aleximage = Aleximage1
-Alfredimage = Alfredimage1
-Anitaimage = Anitaimage1
-Anneimage = Anneimage1
-Bernardimage = Bernardimage1
-Billimage = Billimage1
-Charlesimage = Charlesimage1
-Claireimage = Claireimage1
-Davidimage = Davidimage1
-Ericimage = Ericimage1
-Fransimage = Fransimage1
-Georgeimage = Georgeimage1
-Hermanimage = Hermanimage1
-Joeimage = Joeimage1
-Mariaimage = Mariaimage1
-Maximage = Maximage1
-Paulimage = Paulimage1
-Peterimage = Peterimage1
-Philipimage = Philipimage1
-Richardimage = Richardimage1
-Robertimage = Robertimage1
-Samimage = Samimage1
-Susanimage = Susanimage1
-Tomimage = Tomimage1
+
+Alex = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Aleximage1,command= lambda: click("Alex"))
+Alfred = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Alfredimage1,command=lambda: click("Alfred"))
+Anita = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Anitaimage1,command=lambda: click("Anita"))
+Anne = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Anneimage1,command=lambda: click("Anne"))
+Bernard = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Bernardimage1,command=lambda: click("Bernard"))
+Bill = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Billimage1,command=lambda: click("Bill"))
+Charles = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Charlesimage1,command=lambda: click("Charles"))
+Claire = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Claireimage1,command=lambda: click("Claire"))
+David = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Davidimage1,command=lambda: click("David"))
+Eric = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Ericimage1,command=lambda: click("Eric"))
+Frans = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Fransimage1,command=lambda: click("Frans"))
+George = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Georgeimage1,command=lambda: click("George"))
+Herman = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Hermanimage1,command=lambda: click("Herman"))
+Joe = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Joeimage1,command=lambda: click("Joe"))
+Maria = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Mariaimage1,command=lambda: click("Maria"))
+Max = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Maximage1,command=lambda: click("Max"))
+Paul = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Paulimage1,command=lambda: click("Paul"))
+Peter = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Peterimage1,command=lambda: click("Peter"))
+Philip = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Philipimage1,command=lambda: click("Philip"))
+Richard = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Richardimage1,command=lambda: click("Richard"))
+Robert = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Robertimage1,command=lambda: click("Robert"))
+Sam = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Samimage1,command=lambda: click("Sam"))
+Susan = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Susanimage1,command=lambda: click("Susan"))
+Tom = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Tomimage1,command=lambda: click("Tom"))
 
 
+M1 = person("Alex", Alex, Aleximage1, AleximageX, False, "black", False, True, False, True, False, False, False,0,0)
+M2 = person("Alfred", Alfred, Alfredimage1,AlfredimageX, False, "red", False, True, False, True, False, False, True,0,1)
+M3 = person("Anita", Anita, Anitaimage1,AnitaimageX, False, "blonde", False, False, False, False, False, True, True,0,2)
+M4 = person("Anne",Anne,Anneimage1,AnneimageX, False, "black", False, False, False, False, False, False, False,0,3)
+M5 = person("Bernard",Bernard, Bernardimage1,BernardimageX, True, "brown", False, True, False, False, False, False, False,0,4)
+M6 = person("Bill", Bill, Billimage1, BernardimageX, False, "red", False, True, True, False, True, True, False,0,5)
+M7 = person("Charles",Charles, Charlesimage1, CharlesimageX, False, "blonde", False, True, False, True, False, False, False,0,6)
+M8 = person("Claire", Claire, Claireimage1, ClaireimageX,True, "red", True, False, False, False, False, False, False,0,7)
+M9 = person("David",David, Davidimage1, DavidimageX, False, "blonde", False, True, False, False, True, False, False,1,0)
+M10 = person("Eric",Eric, Ericimage1, EricimageX, True, "blonde", False, True, False, False, False, False, False,1,1)
+M11 = person("Frans",Frans, Fransimage1, FransimageX, False, "red", False, True, False, False, False, False, False,1,2)
+M12 = person("George",George, Georgeimage1, GeorgeimageX, True, "white", False, True, False, False, False, False, False,1,3)
+M13 = person("Herman",Herman,Hermanimage1,HermanimageX, False, "red", False, True, True, False, False, False, False,1,4)
+M14 = person("Joe", Joe, Joeimage1, JoeimageX, False, "blonde", True, True, False, False, False, False, False,1,5)
+M15 = person("Maria",Maria, Mariaimage1, MariaimageX, True, "brown", False, False, False, False, False, False, True,1,6)
+M16 = person("Max",Max, Maximage1, MaximageX, False, "black", False, True, False, True, False, False, False,1,7)
+M17 = person("Paul", Paul, Paulimage1, PaulimageX, False, "white", True, True, False, False, False, False, False,2,0)
+M18 = person("Peter", Peter, Peterimage1, PeterimageX, False, "white", False, True, False, False, False, False, False,2,1)
+M19 = person("Philip", Philip, Philipimage1, PhilipimageX, False, "black", False, True, False, False, True, True, False,2,2)
+M20 = person("Richard", Richard, Richardimage1, RichardimageX, False, "brown", False, True, True, True, True, False, False,2,3)
+M21 = person("Robert",Robert, Robertimage1, RobertimageX, False, "brown", False, True, False, False, False, True, False,2,4)
+M22 = person("Sam",Sam, Samimage1,SamimageX, False, "white", True, True, True, False, False, False, False,2,5)
+M23 = person("Susan",Susan,Susanimage1,SusanimageX, False, "white", False, False, False, False, False, True, True,2,6)
+M24 = person("Tom",Tom,Tomimage1,TomimageX, False, "Black", True, True, True, False, False, False, False,2,7)
 
 
+people=[M1,M2,M3,M4,M5,M6,M7,M8,M9,M10,M11,M12,M13,M14,M15,M16,M17,M18,M19,M20,M21,M22,M23,M24]
+people2=['Alex', 'Alfred', 'Anita', 'Anne', 'Bernard', 'Bill', 'Charles', 'Claire', 'David', 'Eric', 'Frans', 'George', 'Herman', 'Joe', 'Maria', 'Max', 'Paul', 'Peter', 'Philip', 'Richard', 'Robert', 'Sam', 'Susan', 'Tom']
+import random
+choice1 = people[random.randint(0, 23)] #computer picks person at random
+print(choice1.name)
 
+Alex.grid(row=M1.row, column=M1.column)
+Alfred.grid(row=M2.row, column=M2.column)
+Anita.grid(row=M3.row, column=M3.column)
+Anne.grid(row=M4.row, column=M4.column)
+Bernard.grid(row=M5.row, column=M5.column)
+Bill.grid(row=M6.row, column=M6.column)
+Charles.grid(row=M7.row, column=M7.column)
+Claire.grid(row=M8.row, column=M8.column)
+David.grid(row=M9.row, column=M9.column)
+Eric.grid(row=M10.row, column=M10.column)
+Frans.grid(row=M11.row, column=M11.column)
+George.grid(row=M12.row, column=M12.column)
+Herman.grid(row=M13.row, column=M13.column)
+Joe.grid(row=M14.row, column=M14.column)
+Maria.grid(row=M15.row, column=M15.column)
+Max.grid(row=M16.row, column=M16.column)
+Paul.grid(row=M17.row, column=M17.column)
+Peter.grid(row=M18.row, column=M18.column)
+Philip.grid(row=M19.row, column=M19.column)
+Richard.grid(row=M20.row, column=M20.column)
+Robert.grid(row=M21.row, column=M21.column)
+Sam.grid(row=M22.row, column=M22.column)
+Susan.grid(row=M23.row, column=M23.column)
+Tom.grid(row=M24.row, column=M24.column)
 
-
-start=ImageTk.PhotoImage(Image.open("Start.png"))
-
-mainFrame=Frame(root,bg="yellow",width="50",padx=42,pady=3)
-questionFrame=Frame(root,bg="yellow",padx=1,pady=31)
-questionFrame2=Frame(root,bg="yellow",padx=1,pady=31)
-choiceFrame=Frame(root,bg="yellow",pady=3)
-urchar= start
-choice=Label(choiceFrame,bg="yellow",padx=15,pady=15,image=urchar)
-yourcharcter = Label(choiceFrame,text ="Your Person",font='Helvetica 15 bold',bg="yellow",fg="brown")
-choice.pack()
-yourcharcter.pack()
-mainFrame.grid(row=1,column=0,columnspan=3)
-questionFrame.grid(row=2,column=0)
-choiceFrame.grid(row=2,column=1)
-questionFrame2.grid(row=2,column=2)
 
 def guess(name):
     global Aleximage
@@ -348,6 +404,9 @@ def guess(name):
     Sam.grid(row=2, column=5)
     Susan.grid(row=2, column=6)
     Tom.grid(row=2, column=7)
+    if guess == choice1.name:
+        print("winner")
+        reset()
     return(guess)
 
 
@@ -356,7 +415,7 @@ def click(name):
     global choice
     global yourcharcter
     if urchar == start: #CHARECTER SELECTION
-        urchar = name
+        urchar = (people[people2.index(name)].image)
         choice.destroy()
         yourcharcter.destroy()
         choice = Label(choiceFrame, bg="yellow", padx=15, pady=15, image=urchar)
@@ -364,91 +423,67 @@ def click(name):
         choice.pack()
         yourcharcter.pack()
     else:
-        guess(name)
-Alex = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Aleximage,command= lambda: click(Aleximage))
-Alfred = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Alfredimage,command=lambda: click(Alfredimage))
-Anita = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Anitaimage,command=lambda: click(Anitaimage))
-Anne = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Anneimage,command=lambda: click(Anneimage))
-Bernard = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Bernardimage,command=lambda: click(Bernardimage))
-Bill = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Billimage,command=lambda: click(Billimage))
-Charles = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Charlesimage,command=lambda: click(Charlesimage))
-Claire = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Claireimage,command=lambda: click(Claireimage))
-David = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Davidimage,command=lambda: click(Davidimage))
-Eric = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Ericimage,command=lambda: click(Ericimage))
-Frans = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Fransimage,command=lambda: click(Fransimage))
-George = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Georgeimage,command=lambda: click(Georgeimage))
-Herman = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Hermanimage,command=lambda: click(Hermanimage))
-Joe = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Joeimage,command=lambda: click(Joeimage))
-Maria = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Mariaimage,command=lambda: click(Mariaimage))
-Max = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Maximage,command=lambda: click(Maximage))
-Paul = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Paulimage,command=lambda: click(Paulimage))
-Peter = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Peterimage,command=lambda: click(Peterimage))
-Philip = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Philipimage,command=lambda: click(Philipimage))
-Richard = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Richardimage,command=lambda: click(Richardimage))
-Robert = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Robertimage,command=lambda: click(Robertimage))
-Sam = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Samimage,command=lambda: click(Samimage))
-Susan = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Susanimage,command=lambda: click(Susanimage))
-Tom = Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=Tomimage,command=lambda: click(Tomimage))
-Alex.grid(row=0,column=0)
-Alfred.grid(row=0,column=1)
-Anita.grid(row=0,column=2)
-Anne.grid(row=0,column=3)
-Bernard.grid(row=0,column=4)
-Bill.grid(row=0,column=5)
-Charles.grid(row=0,column=6)
-Claire.grid(row=0,column=7)
-David.grid(row=1,column=0)
-Eric.grid(row=1,column=1)
-Frans.grid(row=1,column=2)
-George.grid(row=1,column=3)
-Herman.grid(row=1,column=4)
-Joe.grid(row=1,column=5)
-Maria.grid(row=1,column=6)
-Max.grid(row=1,column=7)
-Paul.grid(row=2,column=0)
-Peter.grid(row=2,column=1)
-Philip.grid(row=2,column=2)
-Richard.grid(row=2,column=3)
-Robert.grid(row=2,column=4)
-Sam.grid(row=2,column=5)
-Susan.grid(row=2,column=6)
-Tom.grid(row=2,column=7)
-def hat()
+        if name == choice1.name:
+            print("winner")
+            #winner() #add winner animation later
+        else:
+            (people[people2.index(name)].variable).destroy()
+            people[people2.index(name)].variable = Button(mainFrame, bg="gray", borderwidth=0, padx=x, pady=y, image=people[people2.index(name)].imageX, state=DISABLED)
+            (people[people2.index(name)].variable).grid(row=(people[people2.index(name)].row),column=(people[people2.index(name)].column))
+
+
+
+def hat():
     global hat
     hat.destroy()
     hat = Button(questionFrame, bg="tan", fg="brown", text="Do they have a hat?",state = DISABLED)
-def glasses()
+    hat.grid(row=2, column=0)
+    if choice1.hat == True:
+        for person in people:
+            if person. hat == False:
+                (person.variable).destroy()
+                person.variable=Button(mainFrame,bg="yellow",borderwidth=0,padx=x,pady=y,image=person.imageX,state=DISABLED)
+                (person.variable).grid(row=person.row,column=person.column)
+def glasses():
     global glasses
     glasses.destroy()
     glasses = Button(questionFrame, bg="tan", fg="brown", text="Do they have glasses?",state = DISABLED)
-def sex()
+    glasses.grid(row=0, column=1)
+def sex():
     global man
     global woman
     man.destroy()
     woman.destroy()
     man = Button(questionFrame, bg="tan", fg="brown", text="Are they a man?    ",state = DISABLED)
     woman = Button(questionFrame, bg="tan", fg="brown", text="Are they a woman?  ",state = DISABLED)
-def bald()
+    man.grid(row=0, column=0)
+    woman.grid(row=1, column=0)
+def bald():
     global bald
     bald.destroy()
     bald = Button(questionFrame, bg="tan", fg="brown", text="Are they bald?",state = DISABLED)
-def beard()
+    bald.grid(row=2, column=1)
+def beard():
     global beard
     beard.destroy()
     beard = Button(questionFrame, bg="tan", fg="brown", text="Do they have a beard?",state = DISABLED)
-def cheeks()
+    beard.grid(row=3, column=1)
+def cheeks():
     global rosiecheeks
     rosiecheeks.destroy()
     rosiecheeks=Button(questionFrame, bg="tan",fg="brown",text="Do they have rosie cheeks?",state = DISABLED)
-def mustache()
+    rosiecheeks.grid(row=1, column=1)
+def mustache():
     global mustache
     mustache.destroy()
     mustache=Button(questionFrame2,bg="tan",fg="brown", text="Do they have a mustache?",state = DISABLED)
-def longhair()
+    mustache.grid(row=0, column=0)
+def longhair():
     global longhair
     longhair.destroy()
     longhair=Button(questionFrame2,bg="tan",fg="brown", text="Do they have long hair?",state = DISABLED)
-def hair(color)
+    longhair.grid(row=1, column=0)
+def hair(color):
     global blonde
     global black
     global red
@@ -457,20 +492,23 @@ def hair(color)
     if color == "blonde":
         blonde.destroy()
         blonde = Button(questionFrame2, bg="tan", fg="brown", text="Do they have blonde hair? ",state = DISABLED)
-
+        blonde.grid(row=2, column=0)
     if color == "black":
         black.destroy()
         black = Button(questionFrame2, bg="tan", fg="brown", text="Do they have black hair?",state = DISABLED )
+        black.grid(row=3, column=0)
     if color == "red":
         red.destroy()
         red = Button(questionFrame2, bg="tan", fg="brown", text="Do they have red hair? ",state = DISABLED)
+        red.grid(row=0, column=1)
     if color == "brown":
         brown.destroy()
         brown = Button(questionFrame2, bg="tan", fg="brown", text="Do they have brown hair? ",state = DISABLED)
+        brown.grid(row=1, column=1)
     if color == "white":
         white.destroy()
         white = Button(questionFrame2, bg="tan", fg="brown", text="Do they have white hair?",state = DISABLED)
-
+        white.grid(row=2, column=1)
 hat=Button(questionFrame,bg="tan",fg="brown", text="Do they have a hat?",command=hat)
 glasses=Button(questionFrame,bg="tan",fg="brown", text="Do they have glasses?",command=glasses)
 man=Button(questionFrame,bg="tan",fg="brown",   text="Are they a man?    ",command=sex)
