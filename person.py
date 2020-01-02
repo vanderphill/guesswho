8fdf45b05ecdb7ad8950b8eb4e3173a1a0b61b3a
vanderphill <51059957+vanderphill@users.noreply.github.com>
2020-01-02 04:17:28 -0600

    logic comps

---
+++ person.py
@@ -0,0 +1,12 @@
+class person:
+    def __init__(self, name, hat, hair_color, glasses, male, bald, mustache, beard, rosie_cheeks, long_hair):
+        self.name = name
+        self.hat = hat
+        self.hair_color = hair_color
+        self.glasses = glasses
+        self.male = male
+        self.bald = bald
+        self.mustache = mustache
+        self.beard = beard
+        self.rosie_cheeks = rosie_cheeks
+        self.long_hair = long_hair
